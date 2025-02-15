9d468ba4dbccac5b96070a6e778e73dc7a23bfd3
Lean <leandergalido328@gmail.com>
2025-02-15 12:51:24 +0800

    Added a signUp feature

---
+++ testing_lab-2/src/App.tsx
@@ -0,0 +1,12 @@
+import CreateAccount from "./components/signup-page/signup-page";
+
+
+function App() {
+  return (
+    <div>
+      <CreateAccount />
+    </div>
+  );
+}
+
+export default App;
---
+++ testing_lab-2/src/components/signup-page/signup-page.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+
+const CreateAccount: React.FC = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    console.log("User Created:", { name, email, password });
+  };
+
+  return (
+    <div>
+      <h2>Create Account</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input
+            type="text"
+            placeholder="Enter your name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input
+            type="email"
+            placeholder="Enter your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            placeholder="Enter your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <br />
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
+};
+
+export default CreateAccount;
